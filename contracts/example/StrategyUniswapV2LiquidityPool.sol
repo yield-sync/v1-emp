673cce979c50b4a4a4394e6f8b199a0a872c8892
harppon <aleem.ahmed2015@gmail.com>
2023-11-18 16:30:09 -0500

    Tabs over spaces

--- contracts/example/StrategyUniswapV2LiquidityPool.sol
+++ contracts/example/StrategyUniswapV2LiquidityPool.sol
@@ -8,16 +8,16 @@ import { Allocation, IYieldSyncV1Strategy } from "../interface/IYieldSyncV1Strat
 
 
 interface IUniswapV2Router {
-    function addLiquidity(
-        address tokenA,
-        address tokenB,
-        uint amountADesired,
-        uint amountBDesired,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB, uint liquidity);
+	function addLiquidity(
+		address tokenA,
+		address tokenB,
+		uint amountADesired,
+		uint amountBDesired,
+		uint amountAMin,
+		uint amountBMin,
+		address to,
+		uint deadline
+	) external returns (uint amountA, uint amountB, uint liquidity);
 }
 
 /**
