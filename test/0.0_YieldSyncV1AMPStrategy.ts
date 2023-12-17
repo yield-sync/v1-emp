4f2944af8e0ef3f58590b931458e6d5f286d5e0f
harppon <aleem.ahmed2015@gmail.com>
2023-12-16 20:32:51 -0500

    Improved

--- test/0.0_YieldSyncV1AMPStrategy.ts
+++ test/0.0_YieldSyncV1AMPStrategy.ts
@@ -1,24 +1,11 @@
-type VaultProperty = {
-	transferDelaySeconds: number,
-	voteAgainstRequired: number,
-	voteForRequired: number,
-};
-
-type UpdateVaultProperty = [
-	// voteAgainstRequired
-	number,
-	// voteForRequired
-	number,
-	// transferDelaySeconds
-	number,
-];
-
-
 const { ethers } = require("hardhat");
 
 
 import { expect } from "chai";
 import { Contract, ContractFactory } from "ethers";
+
+
+const ERROR_STRATEGY_ALREADY_SET = "address(yieldSyncV1AMPStrategyInteractor) != address(0)";
 
 
 describe("[0.0] YieldSyncV1VaultDeployer.sol", async () => {
@@ -27,7 +14,7 @@ describe("[0.0] YieldSyncV1VaultDeployer.sol", async () => {
 	let yieldSyncV1AMPStrategy: Contract;
 
 	beforeEach("[beforeEach] Set up contracts..", async () => {
-		const [, addr1] = await ethers.getSigners();
+		const [owner, addr1] = await ethers.getSigners();
 
 		const MockERC20: ContractFactory = await ethers.getContractFactory("MockERC20");
 		const StrategyInteractorBlank: ContractFactory = await ethers.getContractFactory("StrategyInteractorBlank");
@@ -35,27 +22,59 @@ describe("[0.0] YieldSyncV1VaultDeployer.sol", async () => {
 
 		mockERC20 = await (await MockERC20.deploy()).deployed();
 		strategyInteractorBlank = await (await StrategyInteractorBlank.deploy()).deployed();
-		yieldSyncV1AMPStrategy = await (await YieldSyncV1AMPStrategy.deploy(addr1.address, "Exampe", "EX")).deployed();
+		yieldSyncV1AMPStrategy = await (await YieldSyncV1AMPStrategy.deploy(owner.address, "Exampe", "EX")).deployed();
 	});
 
 	describe("initializeStrategy()", async () => {
 		it(
-			"It should be able to set _strategy and _utilizedERC20",
+			"[auth] Should revert when unauthorized msg.sender calls..",
 			async () => {
 				const [, addr1] = await ethers.getSigners();
 
-				await yieldSyncV1AMPStrategy.connect(addr1).initializeStrategy(
-					strategyInteractorBlank.address,
-					[mockERC20.address]
-				);
+				await expect(
+					yieldSyncV1AMPStrategy.connect(addr1).initializeStrategy(
+						strategyInteractorBlank.address,
+						[mockERC20.address]
+					)
+				).to.be.rejected;
+			}
+		);
 
-				console.log();
+		it(
+			"It should be able to set _strategy and _utilizedERC20..",
+			async () => {
+				await yieldSyncV1AMPStrategy.initializeStrategy(strategyInteractorBlank.address, [mockERC20.address]);
 
 				expect(await yieldSyncV1AMPStrategy.yieldSyncV1AMPStrategyInteractor()).to.be.equal(
 					strategyInteractorBlank.address
 				);
 				expect((await yieldSyncV1AMPStrategy.utilizedERC20()).length).to.be.equal(1);
 				expect((await yieldSyncV1AMPStrategy.utilizedERC20())[0]).to.be.equal(mockERC20.address);
+			}
+		);
+
+		it(
+			"It should be able only be able to set once..",
+			async () => {
+				await yieldSyncV1AMPStrategy.initializeStrategy(strategyInteractorBlank.address, [mockERC20.address]);
+
+				await expect(
+					yieldSyncV1AMPStrategy.initializeStrategy(
+						strategyInteractorBlank.address,
+						[mockERC20.address]
+					)
+				).to.be.rejectedWith(ERROR_STRATEGY_ALREADY_SET);
+			}
+		);
+	});
+
+	describe("utilizedERC20Deposit()", async () => {
+		it(
+			"Should be able to deposit ERC20 into strategy interactor..",
+			async () => {
+				await yieldSyncV1AMPStrategy.initializeStrategy(strategyInteractorBlank.address, [mockERC20.address]);
+
+				await yieldSyncV1AMPStrategy.utilizedERC20Deposit([1])
 			}
 		);
 	});
