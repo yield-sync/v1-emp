6689ee516241e6cbc7cc3a8247483fbfbf45c337
harppon <aleem.ahmed2015@gmail.com>
2023-12-16 20:19:51 -0500

    Updated Tests

--- test/0.0_YieldSyncV1AMPStrategy.ts
+++ test/0.0_YieldSyncV1AMPStrategy.ts
@@ -22,17 +22,20 @@ import { Contract, ContractFactory } from "ethers";
 
 
 describe("[0.0] YieldSyncV1VaultDeployer.sol", async () => {
+	let mockERC20: Contract;
 	let strategyInteractorBlank: Contract;
 	let yieldSyncV1AMPStrategy: Contract;
 
 	beforeEach("[beforeEach] Set up contracts..", async () => {
 		const [, addr1] = await ethers.getSigners();
 
-		const YieldSyncV1AMPStrategy: ContractFactory = await ethers.getContractFactory("YieldSyncV1AMPStrategy");
+		const MockERC20: ContractFactory = await ethers.getContractFactory("MockERC20");
 		const StrategyInteractorBlank: ContractFactory = await ethers.getContractFactory("StrategyInteractorBlank");
+		const YieldSyncV1AMPStrategy: ContractFactory = await ethers.getContractFactory("YieldSyncV1AMPStrategy");
 
-		yieldSyncV1AMPStrategy = await (await YieldSyncV1AMPStrategy.deploy(addr1.address, "Exampe", "EX")).deployed();
+		mockERC20 = await (await MockERC20.deploy()).deployed();
 		strategyInteractorBlank = await (await StrategyInteractorBlank.deploy()).deployed();
+		yieldSyncV1AMPStrategy = await (await YieldSyncV1AMPStrategy.deploy(addr1.address, "Exampe", "EX")).deployed();
 	});
 
 	describe("initializeStrategy()", async () => {
@@ -41,9 +44,18 @@ describe("[0.0] YieldSyncV1VaultDeployer.sol", async () => {
 			async () => {
 				const [, addr1] = await ethers.getSigners();
 
-				await yieldSyncV1AMPStrategy.connect(addr1).initializeStrategy(strategyInteractorBlank.address, []);
+				await yieldSyncV1AMPStrategy.connect(addr1).initializeStrategy(
+					strategyInteractorBlank.address,
+					[mockERC20.address]
+				);
 
-				expect((await yieldSyncV1AMPStrategy.utilizedERC20()).length).to.be.equal(0);
+				console.log();
+
+				expect(await yieldSyncV1AMPStrategy.yieldSyncV1AMPStrategyInteractor()).to.be.equal(
+					strategyInteractorBlank.address
+				);
+				expect((await yieldSyncV1AMPStrategy.utilizedERC20()).length).to.be.equal(1);
+				expect((await yieldSyncV1AMPStrategy.utilizedERC20())[0]).to.be.equal(mockERC20.address);
 			}
 		);
 	});
