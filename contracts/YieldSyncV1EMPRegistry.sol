958d40f87bf38890a9c7e42d79c42e365add65a6
harppon <aleem.ahmed2015@gmail.com>
2024-01-07 15:01:40 -0500

    Updated registry contract

--- contracts/YieldSyncV1EMPRegistry.sol
+++ contracts/YieldSyncV1EMPRegistry.sol
@@ -4,4 +4,14 @@ pragma solidity 0.8.18;
 
 contract YieldSyncV1EMPRegistry
 {
+	mapping (
+		address yieldSynV1EMPDeployer => mapping(address yieldSyncV1EMP => bool valid)
+	) public yieldSynV1EMPDeployer_yieldSyncV1EMP_valid;
+
+
+	function yieldSyncV1EMP_validUpdate(address _yieldSyncV1EMP)
+		public
+	{
+		yieldSynV1EMPDeployer_yieldSyncV1EMP_valid[msg.sender][_yieldSyncV1EMP] = true;
+	}
 }
