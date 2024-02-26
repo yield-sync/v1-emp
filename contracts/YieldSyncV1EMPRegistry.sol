c3ebd6c267dd737d3da875692e538a19aec26ba4
harppon <aleem.ahmed2015@gmail.com>
2024-02-25 23:11:26 -0500

    Commonize pragma version

--- contracts/YieldSyncV1EMPRegistry.sol
+++ contracts/YieldSyncV1EMPRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.18;
 
 
 import { IYieldSyncV1EMPRegistry } from "./interface/IYieldSyncV1EMPRegistry.sol";
--- contracts/example/StrategyInteractorUniswapV2LiquidityPool.sol
+++ contracts/example/StrategyInteractorUniswapV2LiquidityPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.18;
 
 
 import { IYieldSyncV1EMPStrategyInteractor } from "../interface/IYieldSyncV1EMPStrategyInteractor.sol";
