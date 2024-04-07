c8abf29bba6587c1dd15c3d92585fa7435288f13
harppon <aleem.ahmed2015@gmail.com>
2024-04-06 22:55:25 -0400

    Add test for setting strategies on emp

--- test/4.0_YieldSyncV1EMP.ts
+++ test/4.0_YieldSyncV1EMP.ts
@@ -2,18 +2,19 @@ const { ethers } = require("hardhat");
 
 
 import { expect } from "chai";
-import { Contract, ContractFactory } from "ethers";
+import { BigNumber, Contract, ContractFactory } from "ethers";
 
-import { ERROR } from "../common";
+import { ERROR, PERCENT } from "../common";
 
 
 describe("[4.0] YieldSyncV1EMP.sol - Setup", async () =>
 {
 	let eTHValueFeedDummy: Contract;
 	let yieldSyncV1EMP: Contract;
-	let yieldSyncV1EMPRegistry: Contract;
-	let yieldSyncV1EMPStrategyDeployer: Contract;
 	let yieldSyncV1EMPDeployer: Contract;
+	let yieldSyncV1EMPRegistry: Contract;
+	let yieldSyncV1EMPStrategy: Contract;
+	let yieldSyncV1EMPStrategyDeployer: Contract;
 
 
 	beforeEach("[beforeEach] Set up contracts..", async () =>
@@ -21,33 +22,59 @@ describe("[4.0] YieldSyncV1EMP.sol - Setup", async () =>
 		/**
 		* This beforeEach process does the following:
 		* 1) Deploy a registry
-		* 2) deploys an EMP Deployer and registers it on the registry
+		* 2) Deploys an EMP Deployer and registers it on the registry
+		* 3) Attach the deployed emp to a local variable
+		* 4) Deploy a strategy
 		*/
 		const [OWNER] = await ethers.getSigners();
 
 		const YieldSyncV1EMP: ContractFactory = await ethers.getContractFactory("YieldSyncV1EMP");
 		const YieldSyncV1EMPRegistry: ContractFactory = await ethers.getContractFactory("YieldSyncV1EMPRegistry");
 		const YieldSyncV1EMPDeployer: ContractFactory = await ethers.getContractFactory("YieldSyncV1EMPDeployer");
+		const YieldSyncV1EMPStrategyDeployer: ContractFactory = await ethers.getContractFactory("YieldSyncV1EMPStrategyDeployer");
 
 		yieldSyncV1EMPRegistry = await (await YieldSyncV1EMPRegistry.deploy()).deployed();
 		yieldSyncV1EMPDeployer = await (
 			await YieldSyncV1EMPDeployer.deploy(OWNER.address, yieldSyncV1EMPRegistry.address)
 		).deployed();
+		yieldSyncV1EMPStrategyDeployer = await (
+			await YieldSyncV1EMPStrategyDeployer.deploy(OWNER.address, yieldSyncV1EMPRegistry.address)
+		).deployed();
 
 		// Set the EMP Deployer on registry
 		await expect(yieldSyncV1EMPRegistry.yieldSyncV1EMPDeployerUpdate(yieldSyncV1EMPDeployer.address)).to.not.be.reverted;
 
+		// Set the EMP Strategy Deployer on registry
+		await expect(
+			yieldSyncV1EMPRegistry.yieldSyncV1EMPStrategyDeployerUpdate(yieldSyncV1EMPStrategyDeployer.address)
+		).to.not.be.reverted;
+
 		// Deploy an EMP
 		await expect(yieldSyncV1EMPDeployer.deployYieldSyncV1EMP("EMP Name", "EMP")).to.be.not.reverted;
 
-		// Verify that a EMP Strategy has been registered
+		// Verify that a EMP has been registered
 		expect(await yieldSyncV1EMPRegistry.yieldSyncV1EMPId_yieldSyncV1EMP(1)).to.be.not.equal(
 			ethers.constants.AddressZero
 		);
 
-		// Attach the deployed YieldSyncV1EMPStrategy address
+		// Deploy an EMP
+		await expect(
+			yieldSyncV1EMPStrategyDeployer.deployYieldSyncV1EMPStrategy("EMP Strategy Name", "EMPS")
+		).to.be.not.reverted;
+
+		// Verify that a EMP Strategy has been registered
+		expect(await yieldSyncV1EMPRegistry.yieldSyncV1EMPStrategyId_yieldSyncV1EMPStrategy(1)).to.be.not.equal(
+			ethers.constants.AddressZero
+		);
+
+		// Attach the deployed YieldSyncV1EMP address
 		yieldSyncV1EMP = await YieldSyncV1EMP.attach(
 			String(await yieldSyncV1EMPRegistry.yieldSyncV1EMPId_yieldSyncV1EMP(1))
+		);
+
+		// Attach the deployed YieldSyncV1EMPStrategy address
+		yieldSyncV1EMPStrategy = await YieldSyncV1EMP.attach(
+			String(await yieldSyncV1EMPRegistry.yieldSyncV1EMPStrategyId_yieldSyncV1EMPStrategy(1))
 		);
 	});
 
@@ -60,6 +87,22 @@ describe("[4.0] YieldSyncV1EMP.sol - Setup", async () =>
 			await expect(
 				yieldSyncV1EMP.connect(ADDR_1).utilizedYieldSyncV1EMPStrategyUpdate(UtilizedYieldSyncV1EMPStrategy)
 			).to.be.rejectedWith(ERROR.NOT_MANAGER);
+		});
+
+		it("Should allow attaching Strategy to EMP..", async () => {
+			const UtilizedYieldSyncV1EMPStrategy: [string, string][] = [
+				[yieldSyncV1EMPStrategy.address, PERCENT.HUNDRED]
+			];
+
+			await expect(
+				yieldSyncV1EMP.utilizedYieldSyncV1EMPStrategyUpdate(UtilizedYieldSyncV1EMPStrategy)
+			).to.be.not.rejected;
+
+			const strategies: UtilizedYieldSyncV1EMPStrategy[] = await yieldSyncV1EMP.utilizedYieldSyncV1EMPStrategy();
+
+			expect(strategies.length).to.be.equal(1);
+			expect(strategies[0].yieldSyncV1EMPStrategy).to.be.equal(yieldSyncV1EMPStrategy.address);
+			expect(strategies[0].allocation).to.be.equal(PERCENT.HUNDRED);
 		});
 	});
 });
