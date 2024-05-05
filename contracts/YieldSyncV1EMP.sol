d84db29689af252e1c55892b5d139948c9dc6fcb
harppon <aleem.ahmed2015@gmail.com>
2024-05-05 14:24:24 -0400

    Add test for checking invalid withdrawal

--- contracts/YieldSyncV1EMP.sol
+++ contracts/YieldSyncV1EMP.sol
@@ -196,7 +196,6 @@ contract YieldSyncV1EMP is
 
 			require(computed, "!computed");
 
-			// Withdraw the tokens to the user
 			IYieldSyncV1EMPStrategy(_utilizedYieldSyncV1EMPStrategy[i].yieldSyncV1EMPStrategy).utilizedERC20Withdraw(
 				msg.sender,
 				SafeMath.div(SafeMath.mul(utilizedyieldSyncV1EMPStrategyPerToken, _ERC20Amount), 1e18)
