acce23ffda3f2bbba481fda16701b960b9a8af11
Your Name <you@example.com>
2024-09-10 23:24:25 -0400

    Move function to Amounts Validator

--- contracts/V1EMPAmountsValidator.sol
+++ contracts/V1EMPAmountsValidator.sol
@@ -5,7 +5,12 @@ pragma solidity ^0.8.18;
 import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import { IV1EMP } from "./interface/IV1EMP.sol";
-import { IV1EMPAmountsValidator } from "./interface/IV1EMPAmountsValidator.sol";
+import {
+	IV1EMPAmountsValidator,
+	IV1EMPArrayUtility,
+	IV1EMPRegistry,
+	UtilizationERC20
+} from "./interface/IV1EMPAmountsValidator.sol";
 import { IV1EMPETHValueFeed } from "./interface/IV1EMPETHValueFeed.sol";
 import { IV1EMPRegistry } from "./interface/IV1EMPRegistry.sol";
 import { IV1EMPStrategy } from "./interface/IV1EMPStrategy.sol";
@@ -16,9 +21,16 @@ contract V1EMPAmountsValidator is
 {
 	using SafeMath for uint256;
 
+
 	uint256 public constant ONE_HUNDRED_PERCENT = 1e18;
 
-	IV1EMPRegistry public immutable I_V1_EMP_REGISTRY;
+	IV1EMPArrayUtility public immutable override I_V1_EMP_ARRAY_UTILITY;
+	IV1EMPRegistry public immutable override I_V1_EMP_REGISTRY;
+
+
+	mapping (
+		address v1EMP => mapping(address v1EMPStrategy => uint256 utilizedERC20UpdateTracker)
+	) public v1EMP_v1EMPStrategy_utilizedERC20UpdateTracker;
 
 
 	receive ()
@@ -36,6 +48,7 @@ contract V1EMPAmountsValidator is
 	constructor (address _v1EMPRegistry)
 	{
 		I_V1_EMP_REGISTRY = IV1EMPRegistry(_v1EMPRegistry);
+		I_V1_EMP_ARRAY_UTILITY = IV1EMPArrayUtility(I_V1_EMP_REGISTRY.v1EMPArrayUtility());
 	}
 
 
@@ -143,5 +156,94 @@ contract V1EMPAmountsValidator is
 		}
 
 		return valid_;
+	}
+
+	/// @inheritdoc IV1EMPAmountsValidator
+	function utilizedERC20Update(address[] memory _utilizedV1EMPStrategy)
+		public
+		override
+		authEMP()
+		returns (bool updatedRequired_, address[] memory utilizedERC20_, UtilizationERC20[] memory utilizationERC20_)
+	{
+		updatedRequired_ = false;
+
+		uint256 utilizedERC20MaxLength = 0;
+
+		for (uint256 i = 0; i < _utilizedV1EMPStrategy.length; i++)
+		{
+			uint256 utilizedERC20UpdateTracker = IV1EMPStrategy(_utilizedV1EMPStrategy[i]).utilizedERC20UpdateTracker();
+
+			if (v1EMP_v1EMPStrategy_utilizedERC20UpdateTracker[msg.sender][_utilizedV1EMPStrategy[i]] != utilizedERC20UpdateTracker)
+			{
+				updatedRequired_ = true;
+
+				v1EMP_v1EMPStrategy_utilizedERC20UpdateTracker[msg.sender][_utilizedV1EMPStrategy[i]] = utilizedERC20UpdateTracker;
+			}
+
+			utilizedERC20MaxLength += IV1EMPStrategy(_utilizedV1EMPStrategy[i]).utilizedERC20().length;
+		}
+
+		if (!updatedRequired_)
+		{
+			return (updatedRequired_, utilizedERC20_, utilizationERC20_);
+		}
+
+		address[] memory tempUtilizedERC20 = new address[](utilizedERC20MaxLength);
+
+		uint256 utilizedERC20I = 0;
+
+		for (uint256 i = 0; i < _utilizedV1EMPStrategy.length; i++)
+		{
+			address[] memory strategyUtilizedERC20 = IV1EMPStrategy(_utilizedV1EMPStrategy[i]).utilizedERC20();
+
+			for (uint256 ii = 0; ii < strategyUtilizedERC20.length; ii++)
+			{
+				tempUtilizedERC20[utilizedERC20I++] = strategyUtilizedERC20[ii];
+			}
+		}
+
+		tempUtilizedERC20 = I_V1_EMP_ARRAY_UTILITY.removeDuplicates(tempUtilizedERC20);
+		tempUtilizedERC20 = I_V1_EMP_ARRAY_UTILITY.sort(tempUtilizedERC20);
+
+		uint256 utilizedERC20AllocationTotal;
+
+		utilizationERC20_ = new UtilizationERC20[](tempUtilizedERC20.length);
+
+		for (uint256 i = 0; i < _utilizedV1EMPStrategy.length; i++)
+		{
+			IV1EMPStrategy iV1EMPStrategy = IV1EMPStrategy(_utilizedV1EMPStrategy[i]);
+
+			for (uint256 ii = 0; ii < tempUtilizedERC20.length; ii++)
+			{
+				UtilizationERC20 memory utilizationERC20 = iV1EMPStrategy.utilizedERC20_utilizationERC20(tempUtilizedERC20[ii]);
+
+				if (utilizationERC20.deposit)
+				{
+					utilizationERC20_[ii].deposit = true;
+
+					uint256 utilizationERC20Allocation = utilizationERC20.allocation * IV1EMP(
+						msg.sender
+					).utilizedV1EMPStrategy_allocation(
+						_utilizedV1EMPStrategy[i]
+					) / 1e18;
+
+					utilizationERC20_[ii].allocation += utilizationERC20Allocation;
+
+					utilizedERC20AllocationTotal += utilizationERC20Allocation;
+				}
+
+				if (utilizationERC20.withdraw)
+				{
+					utilizationERC20_[ii].withdraw = true;
+				}
+			}
+		}
+
+		require(
+			utilizedERC20AllocationTotal == IV1EMP(msg.sender).ONE_HUNDRED_PERCENT(),
+			"!(utilizedERC20AllocationTotal == IV1EMP(msg.sender).ONE_HUNDRED_PERCENT())"
+		);
+
+		return (updatedRequired_, utilizedERC20_, utilizationERC20_);
 	}
 }
--- contracts/interface/IV1EMPAmountsValidator.sol
+++ contracts/interface/IV1EMPAmountsValidator.sol
@@ -2,8 +2,35 @@
 pragma solidity ^0.8.18;
 
 
+import { IV1EMPArrayUtility } from "./IV1EMPArrayUtility.sol";
+import { IV1EMPRegistry } from "./IV1EMPRegistry.sol";
+import { UtilizationERC20 } from "../struct/UtilizationERC20.sol";
+
+
 interface IV1EMPAmountsValidator
 {
+	/**
+	* @dev [view-IV1EMPArrayUtility]
+	* @notice Implemented IV1EMPArrayUtility
+	* @return {IV1EMPArrayUtility}
+	*/
+	function I_V1_EMP_ARRAY_UTILITY()
+		external
+		view
+		returns (IV1EMPArrayUtility)
+	;
+
+	/**
+	* @dev [view-IV1EMPRegistry]
+	* @notice Implemented IV1EMPRegistry
+	* @return {IV1EMPRegistry}
+	*/
+	function I_V1_EMP_REGISTRY()
+		external
+		view
+		returns (IV1EMPRegistry)
+	;
+
 	/**
 	* @notice Utilized ERC20 Amount Valid
 	* @param _utilizedERC20Amount {uint256}
@@ -14,6 +41,18 @@ interface IV1EMPAmountsValidator
 		external
 		view
 		returns (bool valid_, uint256 utilizedERC20AmountTotalETHValue_)
+	;
+
+	/**
+	* @notice
+	* @return _utilizedV1EMPStrategy {address[]}
+	* @return updatedRequired_ {bool}
+	* @return utilizedERC20_ {address[]}
+	* @return utilizationERC20_ {UtilizationERC20[]}
+	*/
+	function utilizedERC20Update(address[] memory _utilizedV1EMPStrategy)
+		external
+		returns (bool updatedRequired_, address[] memory utilizedERC20_, UtilizationERC20[] memory utilizationERC20_)
 	;
 
 	/**
