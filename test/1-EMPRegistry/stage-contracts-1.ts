a9660df8ced20dff2cf316d98ef86b0d88bf5b8b
Your Name <you@example.com>
2025-02-04 09:20:47 -0500

    Better stageSpecificSetup for stage 1 tests

--- test/1-EMPRegistry/stage-contracts-1.ts
+++ test/1-EMPRegistry/stage-contracts-1.ts
@@ -1,4 +1,4 @@
-import { Contract, VoidSigner } from "ethers";
+import { Contract, VoidSigner, Signer } from "ethers";
 
 import stageContracts, { StageContracts0 } from "../stage-contracts-0";
 import { deployContract } from "../../util/UtilEMP";
@@ -23,29 +23,35 @@ export default async (): Promise<StageContracts1> => {
 
 export async function stageSpecificSetup()
 {
-	const [
-		,
-		,
-		,
-		,
-		fakeERC20,
-		fakeEthValueProvider,
-		fakeStrategyDeployer,
-		fakeStrategyUtility,
-		fakeEMPDeployer,
-		fakeEMPUtility,
-		fakeEMP,
-		fakeEMPStrategy,
-	]: VoidSigner[] = await ethers.getSigners();
+	const CONTRACTS: StageContracts0 = await stageContracts();
+
+	let existingVoidSigners: number = 0;
+
+	let k: keyof StageContracts0;
+
+	for (k in CONTRACTS)
+	{
+		if (Signer.isSigner(CONTRACTS[k]))
+		{
+			existingVoidSigners++;
+		}
+	}
+
+	const signers = await ethers.getSigners();
+
+	if (signers.length < existingVoidSigners + 8)
+	{
+		throw new Error("Not enough signers available");
+	}
 
 	return {
-		fakeERC20,
-		fakeEthValueProvider,
-		fakeStrategyDeployer,
-		fakeStrategyUtility,
-		fakeEMPDeployer,
-		fakeEMPUtility,
-		fakeEMP,
-		fakeEMPStrategy,
+		fakeERC20: signers[existingVoidSigners],
+		fakeEthValueProvider: signers[existingVoidSigners + 1],
+		fakeStrategyDeployer: signers[existingVoidSigners + 2],
+		fakeStrategyUtility: signers[existingVoidSigners + 3],
+		fakeEMPDeployer: signers[existingVoidSigners + 4],
+		fakeEMPUtility: signers[existingVoidSigners + 5],
+		fakeEMP: signers[existingVoidSigners + 6],
+		fakeEMPStrategy: signers[existingVoidSigners + 7],
 	};
 }
