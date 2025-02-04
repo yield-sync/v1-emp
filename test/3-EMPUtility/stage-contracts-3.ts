c5c563e92ef28aa163c48481e1811a3e0df626f4
Your Name <you@example.com>
2025-02-04 09:25:22 -0500

    Update stage-contracts

--- test/3-EMPUtility/stage-contracts-3.ts
+++ test/3-EMPUtility/stage-contracts-3.ts
@@ -1,4 +1,4 @@
-import { Contract } from "ethers";
+import { Contract, Signer } from "ethers";
 
 import { deployContract } from "../../util/UtilEMP";
 import stageContracts, { StageContracts2 } from "../2-EMPStrategyUtility/stage-contracts-2";
@@ -24,11 +24,34 @@ export default async (): Promise<StageContracts3> => {
 
 export async function stageSpecificSetup(registry: Contract)
 {
-	const [, , , , _fakeEMPDeployer, fakeEMP] = await ethers.getSigners();
+	const CONTRACTS: StageContracts2 = await stageContracts();
+
+	let existingVoidSigners: number = 0;
+
+	let k: keyof StageContracts2;
+
+	for (k in CONTRACTS)
+	{
+		if (Signer.isSigner(CONTRACTS[k]))
+		{
+			existingVoidSigners++;
+		}
+	}
+
+	const signers = await ethers.getSigners();
+
+	if (signers.length < existingVoidSigners + 2)
+	{
+		throw new Error("Not enough signers available");
+	}
+
+	const _fakeEMPDeployer = signers[existingVoidSigners];
+
+	const fakeEMP = signers[existingVoidSigners + 1];
 
 	await registry.v1EMPDeployerUpdate(_fakeEMPDeployer.address);
 
 	await registry.connect(_fakeEMPDeployer).v1EMPRegister(fakeEMP.address);
 
-	return { fakeEMP };
+	return { fakeEMP, };
 };
